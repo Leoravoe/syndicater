818ce933ccc10c055b8cc49866c01cbe6770bbe4
SouvikChoudhury360 <72061687+SouvikChoudhury360@users.noreply.github.com>
2021-07-11 00:40:56 +0530

    Update BuyForm.js

--- src/components/BuyForm.js
+++ src/components/BuyForm.js
@@ -62,13 +62,13 @@ class BuyForm extends Component {
           <div className="input-group-append">
             <div className="input-group-text">
               <img src={tokenLogo} height='32' alt=""/>
-              &nbsp; 100 A Tokens
+              &nbsp; A Tokens
             </div>
           </div>
         </div>
         <div className="mb-5">
           <span className="float-left text-muted">Exchange Rate</span>
-          <span className="float-right text-muted">1 ETH = A Tokens</span>
+          <span className="float-right text-muted">1 ETH = 100 A Tokens</span>
         </div>
         <button type="submit" className="btn btn-primary btn-block btn-lg">TRANSACT</button>
       </form>
